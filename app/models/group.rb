b574666724013cb9fbef385977cfb38aef5e5922
NicolasCarrascoHelfer <u202116532@upc.edu.pe>
2024-03-25 22:41:25 -0500

    system date

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,46 @@ class Group < ApplicationRecord
   def next_value
     add = true
     self.segments.order(order: :asc).each do |segment|
-      if add && segment.behavior == "CORRELATIVO" && segment.s_type == "ALFANUMERICO"
+      if segment.s_type == "FECHA" && segment.behavior == "SISTEMA"
+        yearnum = 0
+        placedyear = false
+        jump = false
+        now = Time.now
+        today = ""
+        segment.format.each_char do |character|
+          if character == "Y"
+            yearnum = yearnum + 1
+          end
+        end
+        segment.format.each_char do |character|
+          if !jump
+            case character
+            when "D"
+              today.concat(now.strftime("%d"))
+            when "M"
+              today.concat(now.strftime("%m"))
+            when "Y"
+              if !placedyear
+                if yearnum >= 4
+                  today.concat(now.strftime("%Y"))
+                else
+                  today.concat(now.strftime("%y"))
+                end
+                placedyear = true
+              end
+            end
+            jump = true
+          else
+            jump = false
+          end
+        end
+
+        if segment.value != today
+          segment.update(value: today)
+        end
+      end
+
+      if add && segment.s_type == "ALFANUMERICO" && segment.behavior == "CORRELATIVO"
         plus = true
         newvalue = ""
         segment.value.reverse.each_char do |character|
