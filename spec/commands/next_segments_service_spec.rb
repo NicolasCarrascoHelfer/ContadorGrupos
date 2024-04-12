2410292c1b00fe32317fc2f87cd2c7af580395c5
NicolasCarrascoHelfer <u202116532@upc.edu.pe>
2024-04-12 00:30:32 -0500

    3 first tests

--- spec/commands/next_segments_service_spec.rb
+++ spec/commands/next_segments_service_spec.rb
@@ -1,10 +1,12 @@
+require "rails_helper"
 describe NextSegmentsService do
   subject(:context) { described_class.call(segment, now) }
+  group = Group.create(name: "Testgroup")
 
   describe ".call" do
-    context "when the context is successful" do
-      segment = nil
-      now = nil
+    context "AAA to AAB" do
+      segment = Segment.create(category: "alpha", base_value: "AAA", value: "AAA", behavior: "correlative", reset: "day", position: 1, group: group)
+      now = Time.now.to_date
       let(:segment) { segment }
       let(:now) { now }
 
@@ -13,14 +15,25 @@ describe NextSegmentsService do
       end
     end
 
-    context "when the context is not successful" do
-      segment = nil
-      now = nil
+    context "ZZZ fails" do
+      segment = Segment.create(category: "alpha", base_value: "ZZZ", value: "ZZZ", behavior: "correlative", reset: "day", position: 1, group: group)
+      now = Time.now.to_date
       let(:segment) { segment }
       let(:now) { now }
 
       it "fails" do
         expect(context).to be_failure
+      end
+    end
+
+    context "ZZZ to AAA because reset day" do
+      segment = Segment.create(category: "alpha", base_value: "AAA", value: "ZZZ", behavior: "correlative", reset: "day", position: 1, group: group)
+      now = Time.now.next_day.to_date
+      let(:segment) { segment }
+      let(:now) { now }
+
+      it "succeeds" do
+        expect(context.result).to eq("reset")
       end
     end
   end
