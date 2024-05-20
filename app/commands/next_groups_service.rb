1192e5dbaf3544111cba957d0dc3a1fba40e3656
NicolasCarrascoHelfer <u202116532@upc.edu.pe>
2024-05-19 19:11:54 -0500

    NextGroupsService

---
+++ app/commands/next_groups_service.rb
@@ -0,0 +1,34 @@
+class NextGroupsService
+  prepend SimpleCommand
+
+  def initialize(group, now)
+    @group = group
+    @now = now
+  end
+
+  def call
+    ActiveRecord::Base.transaction do
+      @group.segments.order(position: :asc).each do |segment|
+        next_segment = NextSegmentsService.call(segment, @now)
+
+        if next_segment.success?
+          if next_segment.result
+            segment.update(value: next_segment.result)
+          end
+        else
+          case next_segment.errors.first[1]
+          when "reset"
+            segment.reset_value
+          when "rollback"
+            errors.add(:base, "Error: Se alcanzó el límite del contador")
+            raise ActiveRecord::Rollback
+          when "unchanged"
+            nil
+          else
+            errors.add(:base, next_segment.errors.first[1])
+          end
+        end
+      end
+    end
+  end
+end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,33 +2,14 @@ class GroupsController < ApplicationController
   before_action :set_group
 
   def next
-    previous_value = []
-    rollback = false
     now = Time.now.to_date
 
-    ActiveRecord::Base.transaction do
-      @group.segments.order(position: :asc).each do |segment|
-        next_segment = NextSegmentsService.call(segment, now)
-  
-        if next_segment.success?
-          if next_segment.result
-            segment.update(value: next_segment.result)
-          end
-        else
-          case next_segment.errors.first[1]
-          when "reset"
-            segment.reset_value
-          when "rollback"
-            flash[:alert] = "Error: Se alcanzó el límite del contador"
-            raise ActiveRecord::Rollback
-          when "unchanged"
-            nil
-          else
-            flash[:alert] = next_segment.errors.first[1]
-          end
-        end
-      end
+    next_group = NextGroupsService.call(@group, now)
+
+    if !next_group.success?
+      flash[:alert] = next_group.errors.first[1]
     end
+
     redirect_to @group
   end
 
