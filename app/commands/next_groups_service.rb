b2b09806b648cb1aedefd13683167338ecd0f0e9
NicolasCarrascoHelfer <u202116532@upc.edu.pe>
2024-07-08 22:56:40 -0500

    Services' documentation

--- app/commands/next_groups_service.rb
+++ app/commands/next_groups_service.rb
@@ -1,4 +1,10 @@
 class NextGroupsService
+  # Given a "Group" object and a date as parameters,
+  # it increases the group's counter by one according to
+  # its segments' behavior and reset configuration
+  # considering the specified date.
+  #
+  # If it succeeds, it returns the group's new value.
   prepend SimpleCommand
 
   def initialize(group, now)
@@ -9,6 +15,7 @@ class NextGroupsService
   def call
     ActiveRecord::Base.transaction do
       @group.segments.order(position: :asc).each do |segment|
+        # Checks the selected segment's next value calling NextSegmentValue.
         next_segment = NextSegmentsService.call(segment, @now)
 
         if next_segment.success?
@@ -17,12 +24,15 @@ class NextGroupsService
           end
         else
           case next_segment.errors.first[1]
+          # The segment's value resets to its base value. 
           when "reset"
             segment.reset_value
             segment.update(date: @now.strftime("%Y-%m-%d"))
+          # The transaction is rolled back, the counter doesn't change
           when "rollback"
             errors.add(:base, "Error: Se alcanzó el límite del contador")
             raise ActiveRecord::Rollback
+          # The segment doesn't change
           when "unchanged"
             segment.update(date: @now.strftime("%Y-%m-%d"))
           else
--- app/commands/next_segments_service.rb
+++ app/commands/next_segments_service.rb
@@ -1,4 +1,12 @@
 class NextSegmentsService
+  # Given a "Segment" object and a date as parameters,
+  # it checks if the segment's value will increase
+  # if the counter increases considering the specified
+  # date.
+  #
+  # If it succeeds, it returns the segment's new value.
+  #
+  # If it fails, it returns the reason as an error message.
   prepend SimpleCommand
 
   def initialize(segment, now)
@@ -10,12 +18,15 @@ class NextSegmentsService
     if @segment.alpha? && @segment.correlative?
       next_value = @segment.value.next
       updated = @segment.date.in_time_zone("America/Lima").to_date
+      # Checks if at least a day passed since last update and if the segment can reset.
       if updated != @now && !(@segment.never?)
+        # Checks if the time passed since the last segment's update makes the segment reset to its base value.
         if (@now.year != updated.year && (@segment.year? || @segment.month? || @segment.day?)) || (@now.month != updated.month && (@segment.month? || @segment.day?)) || (@now.day != updated.day && @segment.day?)
           errors.add(:base, "reset")
         else
           return next_value
         end
+      # Checks if the next value for the segment doesn't exceed the character limit.
       elsif next_value.size <= @segment.base_value.size
         return next_value
       else
